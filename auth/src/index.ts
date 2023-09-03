f795eabf182f66575dae29c6216d7b3a1207e435
hari <thisismekrishnan@gmail.com>
2023-09-03 18:12:50 +0530

    Adding mongoose ,Schema,Custom password hashing ,

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
+// Import required modules and libraries
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -9,20 +11,41 @@ import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
+// Create an Express application
 const app = express();
+
+// Middleware for parsing JSON data
 app.use(json());
 
+// Register routers for different routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
+// Handle requests to undefined routes with a Not Found Error
 app.all("*", async (req, res, next) => {
   throw new NotFoundError();
 });
 
+// Use the error handler middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
-});
+// Function to start the server
+const start = async () => {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+
+    // Start the Express server on port 3000
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Start the server
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -14,19 +15,25 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must been 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
 
-    console.log("creating user");
+    const existingUser = await User.findOne({ email });
 
-    throw new DatabaseConnectionError();
-    
-    res.status(201).send(email);
+    if (existingUser) {
+      throw new BadRequestError('Email in use')
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send({ user });
   }
 );
 
