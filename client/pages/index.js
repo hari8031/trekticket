c75f5f4e8f2b185cb39843fcecbef46b2b45c617
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-09-10 00:47:31 +0530

    Reusable Api Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,44 +1,18 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
   console.log(currentUser);
-  if (!currentUser) {
-    // Data is not available yet, return a loading indicator or handle it as needed.
-    return <div>Loading...</div>;
-  }
 
-  // Render the component when data is available
-  return (
-    <div>
-      <h1>Landing Page</h1>
-      {/* Render currentUser data here */}
-    </div>
-  );
+  return <h1>Landing Page</h1>;
 };
 
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === 'undefined') {
-    // Make server-side request and await the result
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: {
-          Host: 'bigapp.com',
-        },
-      }
-    );
-
-    // Return the data as a property
-    return data;
-  } else {
-    // Make client-side request and await the result
-    const { data } = await axios.get('/api/users/currentuser');
-    console.log('components');
-    // Return the data as a property
-    return data;
-  }
+  return data;
 };
-
 
 export default LandingPage;
