e88c0542a0dcde9392dcc5b206e3661c33fe57f6
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-09-18 22:52:03 +0530

    adding listener in tickets-srv

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,30 +1,31 @@
-import request from 'supertest';
-import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
-import { Order, OrderStatus } from '../../models/order';
-import { natsWrapper } from '../../nats-wrapper';
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
+import mongoose from "mongoose";
 
-it('marks an order as cancelled', async () => {
+it("marks an order as cancelled", async () => {
   // create a ticket with Ticket Model
   const ticket = Ticket.build({
-    title: 'concert',
+    title: "concert",
     price: 20,
-    id: ''
+    id: new mongoose.Types.ObjectId().toHexString(),
   });
   await ticket.save();
 
   const user = global.signin();
   // make a request to create an order
   const { body: order } = await request(app)
-    .post('/api/orders')
-    .set('Cookie', user)
+    .post("/api/orders")
+    .set("Cookie", user)
     .send({ ticketId: ticket.id })
     .expect(201);
 
   // make a request to cancel the order
   await request(app)
     .delete(`/api/orders/${order.id}`)
-    .set('Cookie', user)
+    .set("Cookie", user)
     .send()
     .expect(204);
 
@@ -34,26 +35,26 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it('emits a order cancelled event', async () => {
+it("emits a order cancelled event", async () => {
   const ticket = Ticket.build({
-    title: 'concert',
+    title: "concert",
     price: 20,
-    id: ''
+    id: new mongoose.Types.ObjectId().toHexString(),
   });
   await ticket.save();
 
   const user = global.signin();
   // make a request to create an order
   const { body: order } = await request(app)
-    .post('/api/orders')
-    .set('Cookie', user)
+    .post("/api/orders")
+    .set("Cookie", user)
     .send({ ticketId: ticket.id })
     .expect(201);
 
   // make a request to cancel the order
   await request(app)
     .delete(`/api/orders/${order.id}`)
-    .set('Cookie', user)
+    .set("Cookie", user)
     .send()
     .expect(204);
 
