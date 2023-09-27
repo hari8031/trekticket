4a8736619d67ecd19ef86482104c392e5c42968b
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-09-27 15:25:24 +0530

    creating payment routes in payments-srv

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,19 +1,19 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
-import jwt from 'jsonwebtoken';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
-jest.mock('../nats-wrapper');
+jest.mock("../nats-wrapper");
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
   const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
@@ -37,11 +37,11 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   // Build a JWT payload.  { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
-    email: 'test@test.com',
+    id: id || new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
   };
 
   // Create the JWT!
@@ -54,7 +54,7 @@ global.signin = () => {
   const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
